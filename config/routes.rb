ca0f5323955678af4b85a937d5ee79b6debab7ff
lishiyi <lishiyiiii@gmail.com>
2015-06-11 21:08:37 -0400

    Comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 
